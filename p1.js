8f2840b3fc04850f555bd17d7c0e6dad1494b802
SIDDHARTHRAGAVAN <siddharthragavank@gmail.com>
2024-01-21 23:10:57 +0530

    operators

---
+++ p1.js
@@ -0,0 +1,15 @@
+a=10
+console.log("Output of a is",a)
+//arithmetic operators
+
+var b=10
+var c=20
+console.log("Addition : ",b+c);//Addition
+console.log("Subtraction: ",b-c);//Subtraction
+console.log("Multiplication: ",b*c);//Multiplication
+console.log("Division: ",b/c)//Division
+console.log("Modulus: ",b%c)//Modulus
+console.log(typeof(a))
+console.log("Logical and:",b&c)
+console.log("Logical or:",b||c)
+console.log("Logical not:",!a)
