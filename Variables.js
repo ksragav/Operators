df454bc74f6e6147bd2390d43506b2c914e6fca7
SIDDHARTHRAGAVAN <siddharthragavank@gmail.com>
2024-01-21 23:16:34 +0530

    variables

---
+++ Variables.js
@@ -0,0 +1,8 @@
+a='a'
+console.log(a)
+var b='Hello'
+console.log("This variabe belongs to var type",b)
+let c='world'
+console.log("This variabe belongs to var type",c)
+const d=500
+console.log("This variabe belongs to var type",d)
